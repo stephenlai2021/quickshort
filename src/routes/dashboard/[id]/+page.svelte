13e54c967973172ce54bfe20960a213b27e48674
Stephen_Lai <stephenlai2021@gmail.com>
2023-03-26 14:27:10 +0800

    commit

--- src/routes/dashboard/[id]/+page.svelte
+++ src/routes/dashboard/[id]/+page.svelte
@@ -36,22 +36,22 @@
       iconUrl: IconCenter,
       // shadowUrl: "leaf-shadow.png",
 
-      iconSize: [38, 95], // size of the icon
-      shadowSize: [50, 64], // size of the shadow
-      iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
-      shadowAnchor: [4, 62], // the same for the shadow
-      popupAnchor: [-3, -76], // point from which the popup should open relative to the iconAnchor
+      iconSize: [30, 30], // size of the icon
+      // shadowSize: [50, 64], // size of the shadow
+      // iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
+      // shadowAnchor: [4, 62], // the same for the shadow
+      // popupAnchor: [-3, -76], // point from which the popup should open relative to the iconAnchor
     });
 
     const avatarIcon = leaflet.icon({
       iconUrl: IconAvatar,
       // shadowUrl: "leaf-shadow.png",
 
-      iconSize: [38, 95], // size of the icon
-      shadowSize: [50, 64], // size of the shadow
-      iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
-      shadowAnchor: [4, 62], // the same for the shadow
-      popupAnchor: [-3, -76], // point from which the popup should open relative to the iconAnchor
+      iconSize: [30, 30], // size of the icon
+      // shadowSize: [50, 64], // size of the shadow
+      // iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
+      // shadowAnchor: [4, 62], // the same for the shadow
+      // popupAnchor: [-3, -76], // point from which the popup should open relative to the iconAnchor
     });
 
     marker = leaflet
