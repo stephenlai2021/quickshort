74ec99808c4a154a9288a98518ba4f5e88ff0968
Stephen_Lai <stephenlai2021@gmail.com>
2023-03-30 21:56:21 +0800

    commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,11 @@
 <script lang="ts">
-  import { user } from "$lib/stores";
+  import { page } from "$app / stores"
   import Section1 from "$lib/components/page-home/Section1.svelte"
   import Section2 from "$lib/components/page-home/Section2.svelte"
   import Section3 from "$lib/components/page-home/Section3.svelte"
   import Section4 from "$lib/components/page-home/Section4.svelte"
   import Section5 from "$lib/components/page-home/Section5.svelte"
   import Section6 from "$lib/components/page-home/Section6.svelte"
-
-  // import type { PageData } from "./$types";
-  // export let data: PageData;
-  // $user = data.user;  
-  // console.log('user | home client:', $user)
 </script>
 
 <div class="page-home wrapper mt-[70px] w-full">
