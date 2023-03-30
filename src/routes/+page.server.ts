29e5c388159edf2c902922d484842544d844e893
Stephen_Lai <stephenlai2021@gmail.com>
2023-03-30 21:14:46 +0800

    commit

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -2,6 +2,10 @@ import { redirect } from "@sveltejs/kit";
 import type { Actions, PageServerLoad } from "./$types";
 
 export const load: PageServerLoad = async ({ locals }) => {  
+  if (locals.session) {
+    throw redirect(303, "/dashboard");
+  }
+  
   return { user: locals.session };
 };
 
