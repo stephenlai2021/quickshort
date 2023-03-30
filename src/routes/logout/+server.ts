b6c0e9cdaea78e65a10be1139f9980e69aa476c7
Stephen_Lai <stephenlai2021@gmail.com>
2023-03-30 21:10:49 +0800

    commit

--- src/routes/logout/+server.ts
+++ src/routes/logout/+server.ts
@@ -8,6 +8,6 @@ export const POST: RequestHandler = async ({ locals }) => {
     throw error(500, "Something went wrong logging you out.");
   }
 
-  locals.session = null
+  // locals.session = null
   throw redirect(303, "/");
 };
