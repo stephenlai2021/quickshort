258771c6228b05007a7fbe107827f10a2c2a87f6
Stephen_Lai <stephenlai2021@gmail.com>
2023-04-02 19:54:25 +0800

    commit

--- src/routes/logout/+server.ts
+++ src/routes/logout/+server.ts
@@ -5,7 +5,7 @@ export const POST: RequestHandler = async ({ locals }) => {
   const { error: err } = await locals.sb.auth.signOut();
 
   if (err) {
-    // throw error(500, "Something went wrong logging you out.");
+    throw error(500, "Something went wrong logging you out.");
   }
 
   // locals.session = null
