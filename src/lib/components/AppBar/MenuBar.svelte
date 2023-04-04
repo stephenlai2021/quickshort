975ff728ce8ae9ee25f77c67c0cf63d2a805ec2b
Stephen_Lai <stephenlai2021@gmail.com>
2023-04-04 19:14:55 +0800

    commit

--- src/lib/components/AppBar/MenuBar.svelte
+++ src/lib/components/AppBar/MenuBar.svelte
@@ -17,43 +17,44 @@
 
 <div class="bg-base/30 backdrop-blur-2xl fixed top-0 left-0 right-0 z-50">
   <nav class="py-2 relative flex justify-between items-center">
-    <span class="tooltip tooltip-bottom" data-tip={$t('common.home')}>
+    <span class="tooltip tooltip-bottom tooltip-neutral" data-tip={$t('common.home')}>
       <Logo />
     </span>    
 
     <ul class="flex items-center">
       <div class="side-men">
         <ul class="menu menu-horizontal px-1 z-50 flex items-center">
-          <span class="tooltip tooltip-bottom" data-tip={$t('common.theme')}>
+          <div class="tooltip tooltip-bottom tooltip-neutral" data-tip={$t('common.theme')}>
             <ThemeMenu />
-          </span>
-          <span class="tooltip tooltip-bottom" data-tip={$t('common.language')}>
+          </div>
+          <div class="tooltip tooltip-bottom tooltip-neutral" data-tip={$t('common.language')}>
             <LangMenu />
-          </span>
+          </div>
 
           <!-- {#if $page.url.pathname === "/" && !localUser.email} -->
           {#if $page.url.pathname === "/" && !$page.data.user?.user.email}
-              <a href="/auth" class="mr-4 borde max-[530px]:hidden tooltip tooltip-bottom"  data-tip={$t('common.login')}>
+              <!-- <a href="/auth" class="mr-4 borde max-[530px]:hidden tooltip tooltip-bottom"  data-tip={$t('common.login')}> -->
+              <a href="/auth" class="mr-4 borde max-[530px]:hidden tooltip tooltip-bottom tooltip-neutral"  data-tip={$t('common.login')}>
                 <img src={imageLogin} width="24" alt="">
               </a>
           {/if}
 
           <!-- {#if localUser.email && $page.url.pathname !== "/dashboard" && $page.url.pathname !== "/auth"} -->
-          {#if $page.data.user?.user.email && $page.url.pathname !== "/dashboard" && $page.url.pathname !== "/auth"}
+          <!-- {#if $page.data.user?.user.email && $page.url.pathname !== "/dashboard" && $page.url.pathname !== "/auth"}
             <li>
               <a href="/dashboard" class="max-[530px]:hidden pl-0 mr-1  hover:bg-base-100">
                 <img src={imagePanel} width="24" alt="">
               </a>
             </li>
-          {/if}
+          {/if} -->
 
           <!-- {#if localUser}
             <UserMenu {localUser} />
           {/if} -->
 
-          <span class="tooltip tooltip-bottom" data-tip={$t('common.user')}>
+          <div class="tooltip tooltip-bottom tooltip-neutral" data-tip={$t('common.user')}>
             <UserMenu />
-          </span>
+          </div>
         </ul>
       </div>
     </ul>
